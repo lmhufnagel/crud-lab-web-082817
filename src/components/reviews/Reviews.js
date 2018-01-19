26dc72ba96529e28bc60d4f2a15ccd491642ffa6
lmhufnagel <lmhufnagel@gmail.com>
2018-01-19 17:40:50 -0500

    Done.

--- src/components/reviews/Reviews.js
+++ src/components/reviews/Reviews.js
@@ -3,10 +3,18 @@ import Review from './Review';
 
 class Reviews extends Component {
   render() {
+    const { store, restaurantId } = this.props;
+    const associatedReviews = store.getState().reviews.filter(review => review.restaurantId === restaurantId);
+    const reviews = associatedReviews.map((review, index) => {
+      return <Review store={store} key={index} review={review} />
+    })
+
     return (
-      <ul>
-        Reviews
-      </ul>
+      <div>
+        <ul>
+          {reviews}
+        </ul>
+      </div>
     );
   }
 };
--- src/index.js
+++ src/index.js
@@ -1,18 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { App } from './App';
-import createStore from './createStore';
-import manageRestaurant from './reducers/manageRestaurant';
+import createStore from './createStore'
+import manageRestaurant from './reducers/manageRestaurant'
+const store = createStore(manageRestaurant)
+export const renderer = {render: render}
 
-const store = createStore(manageRestaurant);
 
-function render() {
+function render(){
+
   ReactDOM.render(
     <App store={store} />,
     document.getElementById('root')
   );
-};
+}
 
-export const renderer = { render: render };
 
-store.dispatch({ type: '@@init' });
+
+store.dispatch({type: 'buddy'})
